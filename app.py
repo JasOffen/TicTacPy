92fedbcb9bbd314bccf0c5af081d86fd74d6219a
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-08-25 05:42:16 -0700

    Added alternating player turns, and updating cmd line on line 1

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 import string
+line1 = ('_|_|_')
+line2 = ('_|_|_')
+line3 = ('_|_|_')
+is_X = True
 
-def game_loop():
-    print('_|_|_')
-    print('_|_|_')
-    print('_|_|_')
+def game_loop(modifiedline1, modifiedline2, modifiedline3, is_x):
+
+    if is_x == True:
+        playingpiece = 'x'
+    else:
+        playingpiece = 'o'
+
+    print(modifiedline1)
+    print(modifiedline2)
+    print(modifiedline3)
+
     currentMove = input('Make your move! (ex: 1,2): ')
-    print(currentMove)
-    game_loop()
+    if currentMove == "q":
+        quit()
+    if currentMove == "":
+        print('Please make a move')
+        game_loop()
+    if currentMove[2] == '1':
+        if currentMove[0] == '1':
+            modifiedline1 = (playingpiece + modifiedline1[1:5])
+        if currentMove[0] == '2':
+            modifiedline1 = (modifiedline1[0:2] + playingpiece + modifiedline1[3:5])
+        if currentMove[0] == '3':
+            modifiedline1 = (modifiedline1[0:4] + playingpiece)
 
-game_loop()
+    if is_x == True:
+        is_x = False
+    else:
+        is_x = True
+        
+    game_loop(modifiedline1, modifiedline2, modifiedline3, is_x)
+
+game_loop(line1, line2, line3, is_X)
